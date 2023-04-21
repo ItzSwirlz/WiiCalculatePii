68e22a4213ffa3b16d460b60b569b8264c7f9812
Joshua Peisach <itzswirlz2020@outlook.com>
2023-04-20 20:59:15 -0400

    import

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # WiiCalculatePii
 Seeing how many digits of Pi a Wii can calculate (single core IBM PowerPC Broadway CPU) 
+
+## Basic Facts/Useful Information
+
+### Limits (that are relevant)
+- signed int: 2,147,483,647
+- unsigned int: 4,294,967,295
---
+++ source/template.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <gccore.h>
+#include <wiiuse/wpad.h>
+#include <limits.h>
+static void *xfb = NULL;
+static GXRModeObj *rmode = NULL;
+
+//---------------------------------------------------------------------------------
+int main(int argc, char **argv) {
+//---------------------------------------------------------------------------------
+
+	// Initialise the video system
+	VIDEO_Init();
+
+	// This function initialises the attached controllers
+	WPAD_Init();
+
+	// Obtain the preferred video mode from the system
+	// This will correspond to the settings in the Wii menu
+	rmode = VIDEO_GetPreferredMode(NULL);
+
+	// Allocate memory for the display in the uncached region
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+
+	// Initialise the console, required for printf
+	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+
+	// Set up the video registers with the chosen mode
+	VIDEO_Configure(rmode);
+
+	// Tell the video hardware where our display memory is
+	VIDEO_SetNextFramebuffer(xfb);
+
+	// Make the display visible
+	VIDEO_SetBlack(FALSE);
+
+	// Flush the video register changes to the hardware
+	VIDEO_Flush();
+
+	// Wait for Video setup to complete
+	VIDEO_WaitVSync();
+	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+
+
+	// The console understands VT terminal escape codes
+	// This positions the cursor on row 2, column 0
+	// we can use variables for this with format codes too
+	// e.g. printf ("\x1b[%d;%dH", row, column );
+	printf("\x1b[2;0H");
+
+
+	// Gregory-Leibniz series
+	// (4/1) - (4/3) + (4/5) - (4/7) + (4/9)...
+	// Doesn't converge to Pi as quickly as other methods.
+	double ret = 4.0;
+	bool negative = true;
+	for(float i = 3.0; i <= INT_MAX; i += 2.0) {
+		if(negative) {
+			ret -= (4.0/i);
+			negative = false;
+		} else {
+			ret += (4.0/i);
+			negative = true;
+		}
+
+		// see the converging
+
+		// FIXME: printf in c automatically rounds, whether you like it or not.
+		// This makes it hard to see the converging, as eventually you can get
+		// 3.14158 and 3.14160 (examples) just going back and forth without seeing
+		// the rest of what's happening.
+		printf("%lf", ret);
+		printf("\n");
+	}
+
+	// final result
+	printf("%lf", ret);
+
+	while(1) {
+
+		// Call WPAD_ScanPads each loop, this reads the latest controller states
+		WPAD_ScanPads();
+
+		// WPAD_ButtonsDown tells us which buttons were pressed in this loop
+		// this is a "one shot" state which will not fire again until the button has been released
+		u32 pressed = WPAD_ButtonsDown(0);
+
+		// We return to the launcher application via exit
+		if ( pressed & WPAD_BUTTON_HOME ) exit(0);
+
+		// Wait for the next frame
+		VIDEO_WaitVSync();
+	}
+
+	return 0;
+}
